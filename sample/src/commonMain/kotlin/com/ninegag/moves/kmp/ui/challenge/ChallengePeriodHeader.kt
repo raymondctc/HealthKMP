7d9b1f15930069cdc189d05177a2325349f690d6
Raymond Chan <raymond@9gag.com>
2024-11-21 18:03:49 -0800

    clean up

--- sample/src/commonMain/kotlin/com/ninegag/moves/kmp/ui/challenge/ChallengePeriodHeader.kt
+++ sample/src/commonMain/kotlin/com/ninegag/moves/kmp/ui/challenge/ChallengePeriodHeader.kt
@@ -7,10 +7,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDownward
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.ArrowDropUp
-import androidx.compose.material.icons.filled.ArrowUpward
 import androidx.compose.material.icons.filled.CalendarMonth
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
@@ -25,7 +23,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.ninegag.moves.kmp.model.ChallengePeriod
-import com.ninegag.moves.kmp.model.StepTicketBucket
 import com.ninegag.moves.kmp.model.StepTicketBucketUiValues
 
 @Composable
